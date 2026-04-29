5e96ba8dce6e1d08907afd1eb743cb2e8618f31b
AlleyShawwn <cayetuna2000@gmail.com>
2026-04-29 14:44:43 +0800

    update api config

--- config/api.config.ts
+++ config/api.config.ts
@@ -1,7 +1,7 @@
 export default abstract class ApiConfig {
   public static getBaseUrl(isProd: boolean): string {
     return isProd
-      ? "https://velayo-eservice-beta.vercel.app/api"
+      ? "https://velayo-eservice-phi.vercel.app/api"
       : "http://localhost:3000/api";
   }
 }
